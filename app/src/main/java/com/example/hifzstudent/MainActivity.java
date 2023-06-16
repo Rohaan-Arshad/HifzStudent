c752a7dfa254733eeba226fc983720858a5ca2d0
ibi111 <rohaanarshad15@gmail.com>
2023-06-16 22:10:09 +0500

    new activity added

--- app/src/main/java/com/example/hifzstudent/MainActivity.java
+++ app/src/main/java/com/example/hifzstudent/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.hifzstudent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button add, view,git;
+
+
+        add= findViewById(R.id.button);
+        view= findViewById(R.id.button2);
+        git= findViewById(R.id.button3);
+
+        Intent i = new Intent(this,MainActivity2.class);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(i);
+            }
+        });
+
+        //  git coede
+//        Intent gi = new Intent(this,MainActivity2.class);
+//        git.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(gi);
+//            }
+//        });
+
 
 
     }
