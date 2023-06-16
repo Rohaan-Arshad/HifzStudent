3c06aa2d4fdf5e0fa22093a06bc7c93eaa42bf83
ibi111 <rohaanarshad15@gmail.com>
2023-06-16 23:16:04 +0500

    DB class and Student class

--- app/src/main/java/com/example/hifzstudent/MainActivity2.java
+++ app/src/main/java/com/example/hifzstudent/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.hifzstudent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        EditText roll,name,age,cla,surah,ayat,parah;
+
+        roll = findViewById(R.id.editTextTextMultiLine);
+        name = findViewById(R.id.editTextTextMultiLine2);
+        age = findViewById(R.id.editTextTextMultiLine3);
+        cla = findViewById(R.id.editTextTextMultiLine4);
+        parah = findViewById(R.id.editTextTextMultiLine5);
+        surah = findViewById(R.id.editTextTextMultiLine6);
+        ayat = findViewById(R.id.editTextTextMultiLine7);
+
+
+
     }
 }
